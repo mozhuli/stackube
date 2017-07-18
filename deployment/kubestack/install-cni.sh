8a8f15a931a8a73d96eb0f1082741ef3cd0f8ab6
mozhulee <21621232@zju.edu.cn>
2017-07-18 11:02:45 +0800

    Containerize cni network plugin
    including
    * build cni plugin image
    * deploy via kubernetes daemonsets
    
    Change-Id: I9f8c3239aa91c77bfec38728151eeeee5084c20d
    Signed-off-by: mozhuli <21621232@zju.edu.cn>

---
+++ deployment/kubestack/install-cni.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Script to install kubestack CNI on a Kubernetes host.
+# - Expects the host CNI binary path to be mounted at /host/opt/cni/bin.
+# - Expects the host CNI network config path to be mounted at /host/etc/cni/net.d.
+# - Expects the desired CNI config in the CNI_NETWORK_CONFIG env variable.
+# - Expects the desired kubestack config in the KUBESTACK_CONFIG env variable.
+
+# Ensure all variables are defined.
+set -u
+
+# Clean up any existing binaries / config / assets.
+rm -f /host/opt/cni/bin/kubestack
+rm -f /host/etc/cni/net.d/10-kubestack.conf
+rm -f /etc/kubestack.conf
+
+# Place the new binaries if the directory is writeable.
+if [ -w "/host/opt/cni/bin/" ]; then
+	cp /opt/cni/bin/kubestack /host/opt/cni/bin/
+	echo "Wrote kubestack CNI binaries to /host/opt/cni/bin/"
+	echo "CNI plugin version: $(/host/opt/cni/bin/kubestack -v)"
+fi
+
+CNI_NETWORK_CONFIG_FILENAME='/host/etc/cni/net.d/10-kubestack.conf'
+# If specified, overwrite the network configuration file.
+if [ "${CNI_NETWORK_CONFIG:-}" != "" ]; then
+cat >$CNI_NETWORK_CONFIG_FILENAME <<EOF
+${CNI_NETWORK_CONFIG:-}
+EOF
+echo "Wrote CNI config: $(cat /host/etc/cni/net.d/${CNI_NETWORK_CONFIG_FILENAME})"
+fi
+
+KUBESTACK_CONFIG_FILENAME='/etc/kubestack.conf'
+# If specified, overwrite the kubestack configuration file.
+if [ "${KUBESTACK_CONFIG:-}" != "" ]; then
+cat >$KUBESTACK_CONFIG_FILENAME <<EOF
+${KUBESTACK_CONFIG:-}
+EOF
+echo "Wrote CNI config: $(cat /host/etc/cni/net.d/${KUBESTACK_CONFIG_FILENAME})"
+fi
+
+while true; do 
+	sleep 3600; 
+done
